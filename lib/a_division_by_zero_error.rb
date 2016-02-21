8b284efbc2647dcdb432820a96b1ca06669a5ea4
iashy <iashy.ruby@gmail.com>
2016-02-20 23:29:30 -0800

    Done.

--- lib/a_division_by_zero_error.rb
+++ lib/a_division_by_zero_error.rb
@@ -1,3 +1,5 @@
 42 / 6
 
-42 / 0
+42 / 7
+
+#just replaced the fixnum of '0' with that of '7' which allows the division to work.
--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,3 +1,9 @@
 1 + 1
 
-1 + "is the loneliest number"
+"1" + "is the loneliest number"
+
+#or you could put
+
+1 + 1 
+
+#just replaceing the other part of the string with two fixnums or two strings, that way the match 
